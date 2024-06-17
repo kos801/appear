0b8249da26ba866ab5a9c80dfa51ee115f20020c
kos801 <xn83bs@outlook.com>
2024-06-17 15:39:20 +0800

    Modify apply.go

--- apply.go
+++ apply.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# International able wide sometimes space.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
