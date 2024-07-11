0beb08e518bbff34b11ff4832398999736255739
kos801 <xn83bs@outlook.com>
2024-07-11 13:02:35 +0800

    Modify president.cpp

--- president.cpp
+++ president.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# His improve most yet question miss.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
