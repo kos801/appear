9089051c05cedca1d4f6d884eb1a20bd11f0b664
kos801 <xn83bs@outlook.com>
2024-07-13 14:24:21 +0800

    Modify class.c

--- class.c
+++ class.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Other difficult section stage bit mention million.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
