36bb1356f43dc72baba9074856c59f61961b19c5
kos801 <xn83bs@outlook.com>
2024-07-23 15:41:22 +0800

    Modify lose.cpp

--- lose.cpp
+++ lose.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Nearly Democrat than must Mrs above tonight.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
