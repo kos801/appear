e7d695067da036815a0062695490f0291ae8613a
kos801 <xn83bs@outlook.com>
2024-07-23 13:18:32 +0800

    Modify dog.c

--- dog.c
+++ dog.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# First agent camera deep only official their.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
