30569aa2f380124791585e298e560279481f1736
kos801 <xn83bs@outlook.com>
2024-08-03 13:02:54 +0800

    Delete pull.js

--- pull.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Author development current poor assume either create.
-console.log("Hello, World!");
-
-# Over summer which compare.
-console.log("Hello, World!");
