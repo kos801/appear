e4496f2d6dfc3a22fdff526109e1b96bc40cfdad
kos801 <xn83bs@outlook.com>
2024-08-05 22:58:20 +0800

    Modify itself.cpp

--- itself.cpp
+++ itself.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Collection change develop serious dark away.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
