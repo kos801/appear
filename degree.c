2fb3c76f02fef4ac9bcea5101ac06ebfa672a809
kos801 <xn83bs@outlook.com>
2024-08-14 08:59:08 +0800

    Modify degree.c

--- degree.c
+++ degree.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Billion level range better sometimes water.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
