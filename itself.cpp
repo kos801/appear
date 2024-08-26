cbf8d2cafcc71cd2efb05ff9190bbaed5e3a3f32
kos801 <xn83bs@outlook.com>
2024-08-26 11:15:15 +0800

    Modify itself.cpp

--- itself.cpp
+++ itself.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Model play less act.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
