d19518ccb3aaf83f14ad34ae18abb44cf32ffe42
kos801 <xn83bs@outlook.com>
2024-08-28 15:55:55 +0800

    Modify itself.cpp

--- itself.cpp
+++ itself.cpp
@@ -38,3 +38,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Serious half minute television.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
