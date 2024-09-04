8d48cdeb06d6ecd52187aa0ae94c47325435d91b
kos801 <xn83bs@outlook.com>
2024-09-04 13:29:54 +0800

    Modify style.c

--- style.c
+++ style.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Conference fill garden television stay agency.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
