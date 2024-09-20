715b66f3c40c73e1bb7919257e8de4ba923891b4
kos801 <xn83bs@outlook.com>
2024-09-20 19:42:40 +0800

    Modify deep.c

--- deep.c
+++ deep.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Marriage develop prove home media generation spring.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
