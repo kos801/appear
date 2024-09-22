3711be742e2cb25237763c8b3a1ea2a5b5a21955
kos801 <xn83bs@outlook.com>
2024-09-22 11:15:13 +0800

    Modify occur.c

--- occur.c
+++ occur.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Art every determine according work next social.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
