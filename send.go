e1ed6e67616d9318a1fadbccc50be411a5d5571e
kos801 <xn83bs@outlook.com>
2024-10-31 13:21:00 +0800

    Modify send.go

--- send.go
+++ send.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Recently possible laugh not perhaps meeting arrive magazine.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
