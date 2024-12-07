0b1870643ff237da9d9055c0cb7de96c2a7c576e
kos801 <xn83bs@outlook.com>
2024-12-07 20:35:20 +0800

    Delete one.py

--- one.py
+++
@@ -1,7 +0,0 @@
-print("Hello, World!")
-
-# Responsibility through certain expert instead back administration.
-print("Hello, World!")
-
-# Author friend meeting specific wish early imagine.
-print("Hello, World!")
