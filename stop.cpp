b005b3c5ec47d11081462c84ca62194b70c4e1db
kos801 <xn83bs@outlook.com>
2025-01-22 11:23:34 +0800

    Modify stop.cpp

--- stop.cpp
+++ stop.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Night ok health.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
