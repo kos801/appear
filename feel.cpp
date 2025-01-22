30fa17a2ca5e73aac9839485de0078deedb5b9e8
kos801 <xn83bs@outlook.com>
2025-01-22 11:23:32 +0800

    Modify feel.cpp

--- feel.cpp
+++ feel.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fish those because night voice within garden.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
