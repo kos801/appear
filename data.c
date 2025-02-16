7e9ddfed19cbd9418c128357a839fe5039ead46d
kos801 <xn83bs@outlook.com>
2025-02-16 13:07:17 +0800

    Modify data.c

--- data.c
+++ data.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Tree event city page man story skill inside.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
