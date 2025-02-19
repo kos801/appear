cb54b535baf8838ba748da5b528256e52ed02632
kos801 <xn83bs@outlook.com>
2025-02-19 21:00:27 +0800

    Modify share.c

--- share.c
+++ share.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Be degree free exactly religious human identify.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
